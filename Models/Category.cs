524fcd7c4dc1eaf82e3b3fb16646cfa9037c5973
Adan Rahman <167874830+Adan-Rahman-4321@users.noreply.github.com>
2025-01-12 17:01:09 +0500

    Add files via upload

---
+++ Models/Category.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Website.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // Navigation property to show relation with Book
+        public ICollection<Book> Books { get; set; }
+    }
+}
